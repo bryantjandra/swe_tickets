8ad7db270b4fc519bbefcc49f5ab070c157eb2e8
Bryan Tjandra <tjandra.benedict@gmail.com>
2024-12-04 23:09:37 +0700

    add dynamic segments for individual tickets

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 async function getTickets() {
   const response = await fetch("http://localhost:4000/tickets", {
     next: {
-      revalidate: 30,
+      revalidate: 0,
     },
   });
   return response.json();
@@ -14,11 +16,13 @@ async function TicketList() {
     <>
       {tickets.map((ticket) => (
         <div key={ticket.id} className="card my-5">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
-          </div>
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
         </div>
       ))}
 
