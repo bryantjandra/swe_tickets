852deec96d8a92ff644d3879db4a2f05bb2b9d04
Bryan Tjandra <tjandra.benedict@gmail.com>
2024-12-05 00:13:20 +0700

    add static rendering using generateStaticParams

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,9 +1,25 @@
+import { notFound } from "next/navigation";
+
+export const dynamicParms = true;
+
+export async function generateStaticParams() {
+  const response = await fetch("http://localhost:4000/tickets");
+  const tickets = await response.json();
+  return tickets.map((ticket) => ({
+    id: ticket.id,
+  }));
+}
+
 async function getTicket(id) {
   const response = await fetch("http://localhost:4000/tickets/" + id, {
     next: {
       revalidate: 60,
     },
   });
+
+  if (!response.ok) {
+    notFound();
+  }
   return response.json();
 }
 
@@ -29,3 +45,15 @@ async function TicketDetails({ params }) {
 }
 
 export default TicketDetails;
+
+// Here’s how the routing works:
+
+// Static Rendering:
+// At build time, Next.js calls generateStaticParams to generate pre-rendered pages for specific ticket IDs (e.g., /tickets/1).
+
+// Dynamic Rendering (ISR):
+
+// If a user visits a ticket page that wasn’t pre-rendered (e.g., /tickets/5), and dynamicParams is true, Next.js will fetch and render it on-demand.
+// Subsequent visits within 60 seconds will use the cached version. After 60 seconds, a new request will revalidate the page.
+// Error Handling:
+// If the ticket ID doesn’t exist, the notFound() function is called to render the 404 page.
