7d24481e471dea169ee90a3562ac783c2bb2af71
Bryan Tjandra <tjandra.benedict@gmail.com>
2024-12-05 17:20:52 +0700

    creating auth forms

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,7 +1,15 @@
+"use client";
+import AuthForm from "../AuthForm";
+
 function Login() {
+  const handleSubmit = async function (e, email, password) {
+    e.preventDefault();
+    console.log("user log in", email, password);
+  };
   return (
     <main>
       <h2 className="text-center">Log in</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,7 +1,15 @@
+"use client";
+import AuthForm from "../AuthForm";
+
 function Signup() {
+  const handleSubmit = async function (e, email, password) {
+    e.preventDefault();
+    console.log("user signup", email, password);
+  };
   return (
     <main>
       <h2 className="text-center">Sign up</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
