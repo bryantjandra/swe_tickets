24b373b120443d5097f367a13e2169ba464818da
Bryan Tjandra <tjandra.benedict@gmail.com>
2024-12-05 16:44:00 +0700

    add route handler with params

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -11,3 +11,19 @@ export async function GET() {
     status: 200,
   });
 }
+
+export async function POST(request) {
+  const ticket = await request.json();
+
+  const response = await fetch("http://localhost:4000/tickets", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(ticket),
+  });
+
+  const newTicket = await response.json();
+
+  return NextResponse.json(newTicket, {
+    status: 201,
+  });
+}
