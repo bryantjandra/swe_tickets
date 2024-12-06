bafdaeb7cf7415811c5b890f6490704e58a40410
Bryan Tjandra <tjandra.benedict@gmail.com>
2024-12-06 11:32:39 +0700

    add log in and log out capabilities

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,15 +1,33 @@
 "use client";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import AuthForm from "../AuthForm";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 function Login() {
+  const [error, setError] = useState("");
+  const router = useRouter();
   const handleSubmit = async function (e, email, password) {
     e.preventDefault();
-    console.log("user log in", email, password);
+    setError("");
+    const supabase = createClientComponentClient();
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) {
+      setError(error.message);
+    }
+
+    if (!error) {
+      router.push("/");
+    }
   };
   return (
     <main>
       <h2 className="text-center">Log in</h2>
       <AuthForm handleSubmit={handleSubmit} />
+      {error && <div className="error">{error}</div>}
     </main>
   );
 }
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
+import LogoutButton from "./LogoutButton";
 
 function Navbar({ user }) {
   return (
     <nav>
       <h1>SWE Tickets</h1>
       <Link href="/">Dashboard</Link>
-      <Link href="/tickets">Tickets</Link>
+      <Link href="/tickets" className="mr-auto">
+        Tickets
+      </Link>
       {user && <span>Hello, {user.email}</span>}
+      <LogoutButton />
     </nav>
   );
 }
